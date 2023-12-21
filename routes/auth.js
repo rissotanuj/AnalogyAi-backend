b0465703dbc30f55f1a4fa622267529ff7529c9f
rissotanuj <risso.tanuj@gmail.com>
2023-12-21 10:40:55 +0530

    fixing the login

--- routes/auth.js
+++ routes/auth.js
@@ -39,30 +39,35 @@ router.post("/register", async (req, res) => {
 });
 
 // Login
-router.post("/register", async (req, res) => {
+// Login
+router.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    // Check if the email already exists
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({ message: "Email already exists" });
+    // Find the user by email
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials" });
     }
 
-    // Hash the password
-    const saltRounds = 10;
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    // Compare the password
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ message: "Password doesn't match" });
+    }
 
-    // Create a new user
-    const newUser = new User({ email, password: hashedPassword });
-    await newUser.save();
+    // Generate and send JWT token
+    const token = jwt.sign({ userId: user._id }, secretKey, {
+      expiresIn: "1h", // Token expires in 1 hour (adjust as needed)
+    });
 
-    res.status(201).json({ message: "User registered successfully" });
+    res.json({ token, userID: user._id });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "An error occurred" });
   }
 });
+
 
 
 
